72c5bdd90ad0a7f72c8bbadce56d442f1d452d62
Filarh <emiliothedemon@gmail.com>
2026-08-01 01:45:10 -0500

    multi_ir como modulo de Soong, no PRODUCT_COPY_FILES
    AOSP rechaza binarios ELF en PRODUCT_COPY_FILES:
      error: found ELF prebuilt in PRODUCT_COPY_FILES,
             use cc_prebuilt_binary / cc_prebuilt_library_shared instead
    
    Se declara con cc_prebuilt_binary en Android.bp, con compile_multilib 32
    (el binario es ELF32 ARM), strip none y check_elf_files false porque es un
    blob de terceros, y recovery true para que vaya al ramdisk del recovery.
    Los keylayouts y el .rc siguen por PRODUCT_COPY_FILES: son texto.

--- device/allwinner/walley/device.mk
+++ device/allwinner/walley/device.mk
@@ -1,8 +1,12 @@
 LOCAL_PATH := device/allwinner/walley
 
 # Control remoto infrarrojo: binario y keylayouts sacados del recovery de fabrica.
+# El binario va como modulo de Soong (ver Android.bp): PRODUCT_COPY_FILES
+# rechaza los ELF. El .rc es texto y si puede ir por copia.
+TARGET_RECOVERY_DEVICE_MODULES += multi_ir
+PRODUCT_PACKAGES += multi_ir
+
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/system/bin/multi_ir:recovery/root/system/bin/multi_ir \
     $(LOCAL_PATH)/recovery/root/system/etc/init/multi_ir.rc:recovery/root/system/etc/init/multi_ir.rc
 
 PRODUCT_COPY_FILES += $(foreach f,$(wildcard $(LOCAL_PATH)/recovery/root/system/usr/keylayout/*.kl),\
